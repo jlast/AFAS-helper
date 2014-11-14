37744d2a45b7d484ae724c8fd3ab0edd0665bd2c
Jeroen van der Last <j.vanderlast@colours.nl>
2014-11-14 10:20:43 +0100

    Prepare for test

--- js/popup.js
+++ js/popup.js
@@ -17,7 +17,6 @@ function exportEvents()
 	code += "var $wc_projectEdit = $wc_entry.find('#P_C_W_Entry_Detail_EditGrid_re_PrId input');";
 	code += "$wc_projectEdit.val('" + events[0].project + "');";
 	code += "$wc_projectEdit.change();";
-	code += "console.log(GetDomEventData);";
 	code += "var $wc_articleEdit = $wc_entry.find('#P_C_W_Entry_Detail_EditGrid_re_BiId input');";
 	code += "$wc_articleEdit.val('" + events[0].article + "');";
 	code += "$wc_articleEdit.change();";
@@ -25,6 +24,8 @@ function exportEvents()
 	code += "$wc_descriptionEdit.val('" + events[0].title + "');";
 	
 	chrome.tabs.getSelected(null, function(tab){
-		chrome.tabs.executeScript(tab.id, {code: code});
+		chrome.tabs.executeScript(tab.id, {file: "js/libs/jquery-1.4.4.min.js"}, function(){
+			chrome.tabs.executeScript(tab.id, {code: code});
+		});
 	});
 }
